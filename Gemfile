a95f125e54d7e459870e9ed08cbbdcc20c683f8d
isabellarner <larner.isabel@gmail.com>
2017-04-24 20:07:57 +0100

    first commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+source "https://rubygems.org"
+
+# gem "rails"
+gem 'shotgun', '~> 0.9.2'
+gem 'sinatra', '~> 1.4', '>= 1.4.8'
---
+++ app.rb
@@ -0,0 +1,39 @@
+require 'sinatra'
+
+
+get '/' do
+  "Hello Izzy!!!"
+end
+
+get '/secret' do
+  "Izzy and Jaiye's Secret page!!"
+end
+
+get '/another_secret' do
+  "Izzy and Jaiye's Sesond Secret page!!"
+end
+
+get '/yet_another_secret' do
+  "Tired now!!"
+end
+
+get '/random-cat' do
+  @cat_of_many_names = ["Ginger", "Mr. Bigglesworth", "Greebo"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @cat_of_many_names = params[:name]
+  erb(:index)
+end
+
+post '/named-cat' do
+  p params
+  @cat_of_many_names = params[:name]
+  erb(:index)
+end
+
+get '/cat_with_many_names' do
+  erb(:kitty_cat)
+end
